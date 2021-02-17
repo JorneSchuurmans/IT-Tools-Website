b062658f4468d4476d207dd80023e5c45c680a02
JorneSchuurmans <jorne.schuurmans@student.ucll.be>
2021-02-17 10:39:04 +0100

    Made connect4.py simpeler and cleaner

--- modelSolutionsPython/Connect4.py
+++ modelSolutionsPython/Connect4.py
@@ -2,10 +2,9 @@ def toonSpel(veld):
   return "dit staat in de javascript voorgeprogrameerd"
 
 def maakLeegVeld():
-  aantalX, aantalY = (7, 6) 
+      aantalX, aantalY = (7, 6) 
   array = [[0 for i in range(aantalY)] for j in range(aantalX)] 
   return array
-  
 
 def plaatsBlokje(kolom, speler):
   for i in range(6):
@@ -16,70 +15,56 @@ def plaatsBlokje(kolom, speler):
   return False
   
 def checkWin():
-  if checkHorizontaal() or checkVerticaal() or checkDiagonaal():
-    return True
-  return False
+  return checkHorizontaal() or checkVerticaal() or checkDiagonaal()
   
 def checkHorizontaal():
-  teller = 0
   for y in range(6):
+    teller = 0
     for x in range(6):
-      if veld[x][y] == veld[x+1][y] and veld[x][y] != 0:
-        teller+=1
-        if teller >= 3:
-          return True
-      else:
-        teller = 0
+      if veld[x][y] != veld[x+1][y] or veld[x][y] == 0: teller = 0
+      else: teller+=1
+      if teller >= 3: return True
   return False
   
 def checkVerticaal():
   teller = 0
-  for x in range(6):
+  for x in range(7):
+    teller = 0
     for y in range(5):
-      if veld[x][y] == veld[x][y+1] and veld[x][y] != 0:
-        teller+=1
-        if teller >= 3:
-          return True
-      else:
-        teller = 0
+      if veld[x][y] != veld[x][y+1] or veld[x][y] == 0: teller = 0
+      else: teller+=1
+      if teller >= 3: return True
   return False
   
 def checkDiagonaal():
-  teller = 0
   for x in range(4):
     for y in range(3):
-      for i in range(1, 4):
-        if veld[x][y] == veld[x+i][y+i] and veld[x][y] != 0:
-          teller+=1
-          if teller >= 3:
-            return True
-    teller = 0
-    for x in range(4):
-      for y in range(3, 6):
-        for i in range(1, 4):
-          if veld[x][y] == veld[x+i][y-i] and veld[x][y] != 0:
-            teller+=1
-            if teller >= 3:
-              return True
       teller = 0
+      for i in range(1, 4):
+        if veld[x][y] != veld[x+i][y+i] or veld[x][y] == 0: teller = 0
+        else: teller+=1
+        if teller >= 3: return True
+
+  for x in range(4):
+    for y in range(3,6):
+      teller = 0
+      for i in range(1, 4):
+        if veld[x][y] != veld[x+i][y-i] or veld[x][y] == 0: teller = 0
+        else: teller+=1
+        if teller >= 3: return True
   return False
 
 def main():
   speler = 0
   end = False
   while end == False:
-    if speler == 1:
-      speler = 2
-    else:
-      speler = 1
-    test = False
-    while test == False:
-      test = plaatsBlokje(int(input("speler "+str(speler)+":\r"))-1,speler)
+    if speler == 1: speler = 2
+    else: speler = 1
+    while False == plaatsBlokje(int(input("speler "+str(speler)+":\r"))-1,speler): DoNotting()
     end = checkWin()
   print("speler "+str(speler)+" wint\r")
   return
 
-
 veld = maakLeegVeld()
 toonSpel(veld)
-main()  
+main() 
